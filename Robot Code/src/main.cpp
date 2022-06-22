0b7cd7b39756407d4994f6f847f4f214a29db7a5
spencerbradbury <bradburyspencer@gmail.com>
2022-06-21 18:16:29 -0700

    Reflectance sensor + screen

--- Robot Code/src/main.cpp
+++ Robot Code/src/main.cpp
@@ -1,42 +1,22 @@
-/**
-* @brief Blink: Turns on an LED for one second then off for
-* one second and then repeats.
-*/
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
-#include "Arduino.h"
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // This display does not have a reset pin accessible
+#define IR_INPUT PA6
+Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-//Set LED_BUILTIN if it is not defined by Arduino framework
-#define LED_BUILTIN PB2
-
-/**
- * @brief Initialize LED pin as digital write.
- * @param none
- * @retval none
- */
-void setup()
-{
-  //initialize LED digital pin as an output
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display_handler.setTextSize(4);
+  display_handler.setTextColor(SSD1306_WHITE);
+  pinMode(IR_INPUT, INPUT_ANALOG);
 }
 
-/**
- * @brief Turn LED on for 1 sec and off for 1 sec.
- * @param none
- * @retval none
- */
-void loop()
-{
-  uint32_t delay_ms = 1000;
-
-  //turn LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  //wait for one second
-  delay(delay_ms);
-
-  //turn LED off (LOW sets voltage to GND)
-  digitalWrite(LED_BUILTIN, LOW);
-
-  //wait one more second
-  delay(delay_ms);
-}
+void loop() {
+    display_handler.clearDisplay();
+    display_handler.setCursor(0,0);
+    display_handler.println(analogRead(IR_INPUT));
+    display_handler.display();
+};
