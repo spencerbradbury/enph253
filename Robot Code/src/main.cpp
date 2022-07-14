b5f7dd099ebf8f8c9ba66e6c8de583b5f60b9776
spencerbradbury <bradburyspencer@gmail.com>
2022-07-14 12:45:57 -0700

    code for encoders

--- Robot Code/src/main.cpp
+++ Robot Code/src/main.cpp
@@ -6,29 +6,51 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // This display does not have a reset pin accessible
-#define MOTOR_LEFT PA_1
-#define MOTOR_RIGHT PA_2
-#define POT_PIN PA_6
+#define MOTOR_LEFT_F PA_0
+#define MOTOR_LEFT_B PA_1
+#define MOTOR_RIGHT_F PA_2
+#define MOTOR_RIGHT_B PA_3
+#define LEFT_ENCODER PA5
+#define RIGHT_ENCODER PA6
 #define MOTOR_CLOCK_FREQ 100
-
-int value;
 
 Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // void pwm_start(PinName pin, uint32_t clock_freq, uint32_t value, TimerCompareFormat_t resolution){}
 
+int leftCount, rightCount;
+
+void handle_left_motor(){
+  leftCount++;
+}
+
+void handle_right_motor(){
+  rightCount++;
+}
+
 void setup() {
   display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display_handler.setTextSize(4);
   display_handler.setTextColor(SSD1306_WHITE);
-  pinMode(MOTOR_LEFT, OUTPUT);
+  pinMode(MOTOR_LEFT_F, OUTPUT);
+  pinMode(MOTOR_LEFT_B, OUTPUT);
+  pinMode(MOTOR_RIGHT_F, OUTPUT);
+  pinMode(MOTOR_RIGHT_B, OUTPUT);
+  pinMode(LEFT_ENCODER, INPUT_PULLUP);
+  pinMode(RIGHT_ENCODER, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER), handle_left_motor, RISING);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER), handle_right_motor, RISING);
   }
 
 void loop() {
-  value = analogRead(POT_PIN);
   display_handler.clearDisplay();
   display_handler.setCursor(0,0);
-  display_handler.println(value/RESOLUTION_10B_COMPARE_FORMAT);
+  display_handler.print("Left: ");
+  display_handler.println(leftCount);
+  display_handler.print("Right: ");
+  display_handler.println(rightCount);
   display_handler.display();
-  pwm_start(MOTOR_LEFT, MOTOR_CLOCK_FREQ, value, RESOLUTION_10B_COMPARE_FORMAT);
+
+  pwm_start(MOTOR_LEFT_F, MOTOR_CLOCK_FREQ, 512, RESOLUTION_10B_COMPARE_FORMAT);
+  pwm_start(MOTOR_RIGHT_F, MOTOR_CLOCK_FREQ, 512, RESOLUTION_10B_COMPARE_FORMAT);
 };
