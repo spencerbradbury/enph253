9a6fe8e94e2e555d3d71153924f2cacf2e6cd5ad
spencerbradbury <bradburyspencer@gmail.com>
2022-07-21 17:13:16 -0700

    included Encoder header

---
+++ Robot Code/src/MyEncoder.h
@@ -0,0 +1,18 @@
+#define countPerRotation 1441
+
+class MyEncoder{
+    private:
+    int direction;
+    int count;
+    
+    static void onInterrupt(){
+
+    }
+
+    public:
+        MyEncoder(int Pin1, int Pin2){
+            // void onInterrupt();
+            attachInterrupt(digitalPinToInterrupt(Pin1), onInterrupt, RISING);
+        }
+        
+};
