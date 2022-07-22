8659346c3bf21014e870be5eb72d29f8d9b86ba2
spencerbradbury <bradburyspencer@gmail.com>
2022-07-21 17:38:21 -0700

    sweep code for bridge servo

--- Robot Code/src/main.cpp
+++ Robot Code/src/main.cpp
@@ -1,63 +1,22 @@
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
+// #include <Arduino.h>
+#include <Servo.h>
 
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
 
-#define REFLECTANCE_THRESHOLD 300
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // This display does not have a reset pin accessible
-#define REFLECTANCE_INPUT_LEFT PA6
-#define REFLECTANCE_INPUT_RIGHT PA7
-Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+int pos = 0;    // variable to store the servo position
 
 void setup() {
-  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display_handler.setTextSize(1);
-  display_handler.setTextColor(SSD1306_WHITE);
-  pinMode(REFLECTANCE_INPUT_LEFT, INPUT_ANALOG);
-  pinMode(REFLECTANCE_INPUT_RIGHT, INPUT_ANALOG);
-
+  myservo.attach(PA11);  // attaches the servo on pin 9 to the servo object
+  myservo.write(45);
+  delay(3000);
+  for (pos = 45; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
 
 void loop() {
-    bool leftValue = (analogRead(REFLECTANCE_INPUT_LEFT) > REFLECTANCE_THRESHOLD) ? true : false;
-    bool rightValue = (analogRead(REFLECTANCE_INPUT_RIGHT) > REFLECTANCE_THRESHOLD) ? true : false;
-    display_handler.clearDisplay();
-    display_handler.setCursor(0,0);
-    display_handler.print("Left: ");
-    display_handler.println(leftValue);
-    display_handler.print("Right: ");
-    display_handler.println(rightValue);
-    display_handler.display();
-};
+}
 
-
-/* Tape Cases
-x = value to give to pid
-
-Both on tape: x=0
-left off right on: x=-1
-left on right off: x = 1 
-both off (left last on): x = 5
-both off (right last on): x = -5
-*/
-
-
-/*
-loop
-  pot = analog(input); #Feedback Sensor
-  set = knob(); Set point
-
-  error = set - pot; current error
-  p = kp * error; proportional
-  d = kd * (error - lastError); derivative
-  i = ki * error + i; integral
-  i = (i > maxi) ? maxi; check to make integral not dominate
-  i = (i < -maxi) ? -maxi;
-
-  g = p + i + d;
-  motor(3,g); set motor pwm based on pid
-  lastError = error
-
-
-*/
