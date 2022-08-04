50d878b2804822c630a6db26714e09d8e74fb9a3
TWilson05 <90281262+TWilson05@users.noreply.github.com>
2022-08-03 19:58:46 -0700

    tyler's first commit

--- Robot Code/src/main.cpp
+++ Robot Code/src/main.cpp
@@ -7,6 +7,8 @@
 #include "IR.h"
 #include "Claw.h"
 
+//Tyler's first commit
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1    // This display does not have a reset pin accessible
