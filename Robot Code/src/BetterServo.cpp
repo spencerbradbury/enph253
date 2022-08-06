210cb0983b10e58f3e7762d487b858f526f4d122
spencerbradbury <bradburyspencer@gmail.com>
2022-08-05 17:18:35 -0700

    changed angle map values to better reflect what is seen in the real world

--- Robot Code/src/BetterServo.cpp
+++ Robot Code/src/BetterServo.cpp
@@ -10,6 +10,6 @@ BetterServo::BetterServo(PinName servoPin)
 
 void BetterServo::write(int angle)
 {
-    int dutyCycle = map(angle, -90, 90, 800, 2200);
+    int dutyCycle = map(angle, -90, 90, 600, 2400);
     pwm_start(servoPin, 50, map(dutyCycle, 0, 20000, 0, 16384), RESOLUTION_14B_COMPARE_FORMAT);
 }
