ed115b37c541754bd2e7510568d6b08202d82c01
spencerbradbury <bradburyspencer@gmail.com>
2022-08-10 15:44:09 -0700

    moved states enum to header to allow for access between files

---
+++ Robot Code/src/main.h
@@ -0,0 +1,15 @@
+#if !defined(MAIN_H)
+#define MAIN_H
+
+enum States
+{
+    TapeFollow,
+    ChickenWire,
+    FindTape,
+    PassArch,
+    IRFollow,
+    Bridge,
+    DropIdols,
+};
+
+#endif // MAIN_H
