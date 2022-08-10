dd804eea0e688d49589d300c74b24a700f5a58b7
spencerbradbury <bradburyspencer@gmail.com>
2022-08-10 13:49:55 -0700

    some turning comments

--- Robot Code/src/Encoder.h
+++ Robot Code/src/Encoder.h
@@ -3,6 +3,22 @@
 
 #include <Arduino.h>
 
+/*Spin Test Results
+- 700 = 90 almost exactly
+- 225 ~= 33
+- 400 = 51
+- 335 = 48
+
+good enough to just use the same numbers based of left encoder to turn right    
+
+
+
+- 220 Left = ~40 degrees
+- 900 LEft = ~120 deg
+- 1200 Left = ~150 deg
+
+*/
+
 #define COUNT_PER_ROTATION 1441.0      
 #define WHEEL_DIAMTER 111.0             // mm or 11.1cm
 #define DISTANCE_PER_COUNT PI * WHEEL_DIAMTER / COUNT_PER_ROTATION // mm 0.241996
